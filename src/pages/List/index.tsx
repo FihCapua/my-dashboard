bb8108cbe7b01f1cc190a518df6328e045572793
FihCapua <fihcapua@gmail.com>
2021-07-13 16:08:14 -0300

    (feat) create filters for current and eventual finances

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -2,84 +2,44 @@ import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
-import { Container, Content } from './styles';
+import { Container, Content, Filter } from './styles';
 
 const List: React.FC = () => {
-    const options = [
-        { value: 'Teste', label: 'Teste' },
-        { value: 'Teste2', label: 'Teste2' },
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
+    ];
+
+    const years = [
+        { value: 2021, label: 2021 },
+        { value: 2022, label: 2022 },
+        { value: 2023, label: 2023 },
     ];
 
     return (
         <Container>
             <ContentHeader title="List" lineColor="#F7931B">
-                <SelectInput options={options} />
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
 
+            <Filter>
+                <button
+                    type="button"
+                    className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button
+                    type="button"
+                    className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filter>
+
             <Content>
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
-                <HistoryFinanceCard
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="13/07/2021"
-                    amount="R$ 130,00"
-                />
-
                 <HistoryFinanceCard
                     tagColor="#E44C4E"
                     title="Conta de Luz"
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -3,3 +3,39 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Content = styled.div``;
+
+export const Filter = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+
+    .tag-filter {
+        font-size: 16px;
+        font-weight: 500;
+        background: none;
+        color: ${(props) => props.theme.colors.white};
+        margin: 0 10px;
+        transition: opacity 0.3s;
+
+        &:hover {
+            opacity: 0.7;
+        }
+    }
+
+    .tag-filter-recurrent::after {
+        content: '';
+        display: block;
+        width: 55px;
+        margin: 3px auto;
+        border-bottom: 5px solid ${(props) => props.theme.colors.warning};
+    }
+
+    .tag-filter-eventual::after {
+        content: '';
+        display: block;
+        width: 55px;
+        margin: 3px auto;
+        border-bottom: 5px solid ${(props) => props.theme.colors.success};
+    }
+`;
