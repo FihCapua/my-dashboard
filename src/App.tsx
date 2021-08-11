23ae59013e4251a7badee5aae0320edaf9954522
FihCapua <fihcapua@gmail.com>
2021-08-11 15:27:44 -0300

    (feat) create hook theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Routes from './routes';
 
-import dark from './styles/themes/dark';
+import { useTheme } from './hooks/theme';
 
 const App: React.FC = () => {
+    const { theme } = useTheme();
     return (
-        <ThemeProvider theme={dark}>
+        <ThemeProvider theme={theme}>
             <GlobalStyles />
             <Routes />
         </ThemeProvider>
