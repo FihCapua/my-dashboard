82ecfa03bfd3cea99ad088b73e62a0059b5414d5
FihCapua <fihcapua@gmail.com>
2021-08-16 17:25:12 -0300

    (feat) login screen created

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import LogoImg from '../../assets/logo.svg';
+import { Container, Logo, Form, FormTitle } from './styles';
 
 const SignIn: React.FC = () => {
     return (
-        <h1>SignIn</h1>
+        <Container>
+            <Logo>
+                <img src={LogoImg} alt="Minha Carteira" />
+                <h1>Minha Carteira</h1>
+            </Logo>
+
+            <Form>
+                <FormTitle>Entrar</FormTitle>
+
+                <input type="text"></input>
+                <input type="text"></input>
+
+                <button type="submit">Acessar</button>
+            </Form>
+        </Container>
     );
-} 
+};
 
 export default SignIn;
