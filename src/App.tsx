77099e0a5dd8b0b88394480787cd0452407b7a64
FihCapua <fihcapua@gmail.com>
2023-04-02 17:48:23 -0300

    feat: create color theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from "styled-components"
 import { Layout } from "./components/Layout"
 import GlobalStyles from "./styles/GlobalStyles"
+import dark from "./styles/themes/dark"
 
 const App: React.FC = () => {
     return(
-        <>        
+        <ThemeProvider theme={dark}>        
             <GlobalStyles />
             <Layout />
-        </>
+        </ThemeProvider>
     )
 }
 
