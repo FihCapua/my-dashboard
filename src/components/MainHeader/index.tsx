aac0af5589f265bd86ccbcefe74bed69492d5012
FihCapua <fihcapua@gmail.com>
2023-04-02 18:23:03 -0300

    feat: create name and emoji in header component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,7 +1,28 @@
-import { Container } from "./style"
+import { useMemo } from "react";
+import { 
+    Container,
+    Profile,
+    Welcome,
+    Username
+} from "./style";
+
+import emojis from '../../utils/emojis'
 
 export const MainHeader: React.FC = () => {
-    return(
-        <Container>Hello Dashboard!</Container>
-    )
-}
+    const emoji = useMemo(() => {
+        const index = Math.floor(Math.random() * emojis.length)
+
+        return emojis[index]
+    }, [])
+
+  return (
+    <Container>
+      <h1>Toggle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <Username>Fih</Username>
+      </Profile>
+    </Container>
+  );
+};
